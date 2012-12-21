7c7a2bb50f382853b4aba2a70e80d3a12c3d26a4
Brandon Martel <brandonmartel@gmail.com>
2012-12-21 14:42:14 -0600

    modified RPG class library

--- RPGLibrary/Properties/AssemblyInfo.cs
+++ RPGLibrary/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RPGLibrary")]
-[assembly: AssemblyProduct("RPGLibrary")]
+[assembly: AssemblyTitle("XRpgLib")]
+[assembly: AssemblyProduct("XRpgLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
